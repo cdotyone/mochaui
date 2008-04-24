151a2e8624f0b6e92288fc122adb3a8aad30cee0
Gregory Houston <gregory.houston@gmail.com>
2008-04-24 07:51:00 +0000

    Work on collapsible windows.

--- mocha-ui/scripts/source/Core/Core.js
+++ mocha-ui/scripts/source/Core/Core.js
@@ -113,9 +113,11 @@ var MochaUI = new Hash({
 	},
 	collapseToggle: function(windowEl){
 		var instances = MochaUI.Windows.instances;
-		var currentInstance = instances.get(windowEl.id);		
+		var currentInstance = instances.get(windowEl.id);
+		var handles = currentInstance.windowEl.getElements('.handle');		
 		if (currentInstance.isCollapsed == false) {
 			currentInstance.isCollapsed = true;
+			handles.setStyle('display', 'none');
 			if ( currentInstance.iframe ) {
 				currentInstance.iframeEl.setStyle('visibility', 'hidden');
 			}			
@@ -138,7 +140,8 @@ var MochaUI = new Hash({
 			});
 			if ( currentInstance.iframe ) {
 				currentInstance.iframeEl.setStyle('visibility', 'visible');
-			}			
+			}
+			handles.setStyle('display', 'block');			
 		}		
 	},
 	/*
