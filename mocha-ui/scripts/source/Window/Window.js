5cff9cfa2f6481cb8a36167ef29fa54de1c52eea
Gregory Houston <gregory.houston@gmail.com>
2008-08-13 11:16:12 +0000

    Work on layout and style.

--- mocha-ui/scripts/source/Window/Window.js
+++ mocha-ui/scripts/source/Window/Window.js
@@ -1470,7 +1470,7 @@ MochaUI.Window = new Class({
 					(height  + options.headerHeight) * .5 + shadowOffset.x,
 					(width *.5) - (x * 2) - shadowOffset.x,
 					[0, 0, 0],
-					x == shadowBlur ? .7 : .08 + (x * .04)
+					x == shadowBlur ? .75 : .075 + (x * .04)
 				);
 			}
 		}
