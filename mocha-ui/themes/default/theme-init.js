952e85c1089253ab21062cd09b64d2a4413f93d1
Gregory Houston <gregory.houston@gmail.com>
2009-02-16 04:14:34 +0000

    Committing theme assets.

--- mocha-ui/themes/default/theme-init.js
+++ mocha-ui/themes/default/theme-init.js
@@ -14,16 +14,12 @@ MochaUI.options.stylesheets = [];
 Window Options  
 ---------------------------------------------------------------- */
 
-var newWindowOptions = {
-	// headerStartColor: [0, 200, 0]
-};
+MochaUI.newWindowOptions = new Hash({
+
+});
 
 /*  
 Change Themes 
 ---------------------------------------------------------------- */
-
-// Reset original options
-$extend(MochaUI.Windows.windowOptions, MochaUI.Windows.windowOptionsOriginal);
-$extend(MochaUI.Windows.windowOptions, newWindowOptions);
 
 MochaUI.themeChange();
--- mocha-ui/themes/test/theme-init.js
+++ mocha-ui/themes/test/theme-init.js
@@ -8,24 +8,27 @@ Theme: Test
 Stylesheets  
 ---------------------------------------------------------------- */
 
-MochaUI.options.stylesheets = ['content.css'];
+MochaUI.options.stylesheets = [];
 
 /*  
 Window Options  
 ---------------------------------------------------------------- */
 
-var newWindowOptions = {
-	headerStartColor: [250, 200, 200],
-	headerStopColor: [250, 250, 250],
-	bodyBgColor: [245, 245, 245]		
-};
+MochaUI.newWindowOptions = new Hash({
+	headerStartColor: [90, 90, 90],
+	headerStopColor:  [65, 65, 65],
+	bodyBgColor:      [71, 71, 71],
+	minimizeBgColor:  [100, 100, 100],
+	minimizeColor:    [250, 250, 250],
+	maximizeBgColor:  [100, 100, 100],
+	maximizeColor:    [250, 250, 250],
+	closeBgColor:     [100, 100, 100],
+	closeColor:       [250, 250, 250],
+	resizableColor:   [254, 254, 254]		
+});
 
 /*  
 Change Themes 
 ---------------------------------------------------------------- */
-
-// Reset original options
-$extend(MochaUI.Windows.windowOptions, MochaUI.Windows.windowOptionsOriginal);
-$extend(MochaUI.Windows.windowOptions, newWindowOptions);
 
 MochaUI.themeChange();
