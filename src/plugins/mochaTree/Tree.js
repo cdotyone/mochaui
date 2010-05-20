74e197fc6e7b56b77e2c142fcf77fa549cc41d03
Chris Doty <cdoty1@yahoo.com>
2010-05-20 07:02:34 -0500

    first draft of mochaui tree done - no testing performed

--- src/plugins/mochaTree/Tree.js
+++ src/plugins/mochaTree/Tree.js
@@ -18,15 +18,16 @@ MUI.Tree = new Class({
 Implements: [Events, Options],
 
 options: {
-     cssClass:      'treeView'
+     cssClass:      'tree'
     ,nodes:         $A([])
+    ,showCheckBox:  true
     ,textField:     'text'
     ,valueField:    'value'
     ,tipField:      'tip'
     ,selectedValue: ''
     ,selectedNode:  null
     ,expanded:      null
-    ,parentElement: false
+    ,container:     null
     ,depth:         2
     ,id: ''
     
@@ -49,70 +50,167 @@ initialize: function( options )
     MUI.set(id,this);    
 },
 
-toDOM: function() 
+toDOM: function(containerEl)
 {
     var self=this;        
     var o=self.options;
-    
-    var d=$(o.id);
-    var u;
-    if(!d) { 
-        d=new Element('div');
-        d.id=o.id;
-    } else u=d.getElement('ul');
-    if(!u) {
-        u=new Element('ul');
-        d.appendChild(u);    
-    } else u.empty();
+
+    var isNew = false;
+    var div=$(o.id);
+    var ul;
+    if(!div) { 
+        div=new Element('div');
+        div.id=o.id;
+        isNew=true;
+    } else ul=div.getElement('ul');
+    if(!ul) {
+        ul=new Element('ul');
+        div.appendChild(ul);    
+    } else ul.empty();
     if(o.cssClass) {
-        d.set('class',o.cssClass);
-        u.set('class',o.cssClass);
+        div.set('class',o.cssClass);
+        ul.set('class',o.cssClass);
     }
-    self.element = d;   
+    self.element = div;   
     
     var nodes=o.nodes;
     if(o.nodes.length>1) {
-        var l=new Element('li',{styles:{'border':'solid 1px white;'}}).inject(u);
-        u=new Element('ul').inject(l);
+        var li=new Element('li',{styles:{'border':'solid 1px white;'}}).inject(ul);
+        ul=new Element('ul').inject(li);
     }
-    nodes.each(function(node) { self.buildNode(node,self,u); });
-    var last=u.getChildren().getLast();
+    nodes.each(function(node) { self.buildNode(node,self,ul); });
+    var last=ul.getChildren().getLast();
     if(last) last.addClass('last');
       
-    if(o.parentElement) o.parentElement.appendChild(d);
-    self.options.depth=0;
-    
-    return d;
+    o.depth=0;
+
+    if(!isNew) return this;
+
+    window.addEvent('domready', function() {
+        var container=$(containerEl ? containerEl : o.container);
+        container.appendChild(div);
+    });
+
+    return div;
 },
 
 selectValue: function(val,e) 
 {
-    var o=this;
-    var n=o.nodeFind(o,val);
-    o=o.options;
-    if(n && n.DOM) {
-        if(o.selectedNode && o.selectedNode.DOM) o.selectedNode.DOM.getElement('a').className='';
-        o.selectedNode = n;
-        n.DOM.getElement('a').className = 'sel';
+    var self=this;
+    var node=self.nodeFind(val);
+    self=self.options;
+    if(node && node.DOM) {
+        if(self.selectedNode && self.selectedNode._element) self.selectedNode._element.getElement('a').removeClass('sel');
+        self.selectedNode = node;
+        node._element.getElement('a').addClass('sel');
     }
-    o.selectedValue = val;
-    if(n) this.fireEvent('onNodeSelected',[e,n]);
+    self.selectedValue = val;
+    if(node) this.fireEvent('onNodeSelected',[e,node]);
 },
 
-updateNode: function(data) 
+updateNode: function(node) 
 {
-    if(!data) return;
+    if(!node) return;
     
-    var o=this;
-    var n=o.nodeFind(o,data.value);
+    var self=this;
+    var n=self.nodeFind(node.value);
     if(n) {    
-        var dom = n.DOM;
-        n.fromJSON(data);
-        if(dom) {         
-            n.toDOM(false,dom);
-            o.setLast(dom.getParent());
+        var el = node._element;
+        n.fromJSON(node);
+        if(el) {
+            self.buildNode(node,el.getParent());
+            self.setLast(el.getParent());
         }
     }   
+},
+
+buildNode: function(node, parent, depth) {
+    var self = this;
+    var o=this.options;
+    if(!depth) depth=1;
+
+    var a, span, t, ul, chk, li;
+    if(node._element!=null) {li=node._element;}
+    if(!li) li = new Element('li');
+    else li.empty();
+
+    if(parent) {
+        if(parent._ul) li.inject(parent._ul);
+        else li.inject(parent); 
+    }
+
+    if(o.showCheckBox) chk = new Element('INPUT', { 'type':'checkbox', 'value': node.value });
+    a = new Element('a',{'href':'#' + node.value}).inject(li);
+    span = new Element('span',{'text':node.text}).inject(a);
+
+    self._element = li;
+    if (node.tip) a.title = node.tip;
+
+    if (chk) chk.checked = node.checked;
+
+    if (o.selectedValue == node.value) {
+        self.element.getElements('.sel').removeClass('sel');
+        a.className = 'sel';
+        o.selectedNode = self;
+    }
+
+    if (node.image) {
+        span.style.background = 'transparent url(' + node.image + ') no-repeat scroll left top';
+        span.style.paddingLeft = '20px';
+    }
+
+    if (node.nodes && node.nodes.length > 0) {
+        if (!ul) ul = new Element('ul').inject(li);
+        if (node.expanded == 0 && o.depth<=depth) ul.style.display = 'none';
+        node._ul =ul;
+        
+        for (var i = 0; i < node.nodes.length; i++) {
+            self.buildNode(node.nodes[i],node,ul,depth+1);
+        }
+        ul.childNodes[ul.childNodes.length - 1].addClass('last');
+    }
+    if (node.expanded != 0) li.set('class','O');
+    else li.set('class','C');
+    if ((!node.nodes || node.nodes.length == 0) && node.hasChildren == 0) li.set('class','nochild');
+
+
+    // set events
+    if (chk) {
+        chk.removeEvents('click');
+        chk.addEvent('click', function(e) { self.onNodeCheck(node,e); });
+    }
+    li.removeEvents('click');
+    li.addEvent('click', function(e) { o.onNodeExpand(node,e); });
+    a.removeEvents('click');
+    a.addEvent('click', function(e) { o.onNodeClick(node,e); });
+
+    return self;
+},
+
+nodeFind: function(val,node) {
+    if (node!=null && node.options!=null && node.value && node.value + '' == val + '') { return node; }
+    var nodes;
+    if(node.options) nodes=node.options.nodes;
+    if(nodes==null) nodes=node.nodes;
+    if (nodes) {
+        var l = nodes.length;
+        for (var i = 0; i < l; i++) {
+            var snode = this.nodeFind(val,nodes[i]);
+            if (snode) return snode;
+        }
+    }
+    return null;
+},
+
+nodeRefresh: function(node) {
+    if(!node._element) return;
+    this.buildNode(node,node._element.getParent());
+    this.setLast(node._element.getParent());
+},
+
+nodeGetPath: function(node) {
+    if(!node._parent) return node.value;
+    return this.getPath(node._parent)+'/'+node.value;
 },
 
 setLast: function(node) 
@@ -129,204 +227,57 @@ setLast: function(node)
             }
         }
     }
-}
-    
-});
+},
 
+onNodeExpand: function(node,e) {
+    var self = this;
+    var o=self.options;
 
-/*
-MUI.TreeItem = new Class({
-    Implements: [Options],
-
-    options: {
-         text:          ''
-        ,value:         ''
-        ,expanded:      0
-        ,hasChildren:   0
-        ,selected:      0
-        ,checked:       0
-        ,tip:           null
-        ,image:         null
-        ,nodes:         $A([])
-        ,data:          null
-    },
-
-    initialize: function(options,tree,parentNode) {
-        this.setOptions(options);
-        this.Tree=tree;
-        this.data=options;
-        this.ParentNode=parentNode;
-        if(options) {
-            var topt=this.Tree.options;
-            if(!options.text) this.options.text=options[topt.TextField];
-            if(!options.value) this.options.value=options[topt.ValueField];
-            if(!options.tip) this.options.tip=options[topt.TipField];
-            this.setNodes(options.nodes);
-        }
-    },
-
-    setNodes: function(nodes) {
-        if (!nodes) return;
-        var o=this;
-
-        var options = o.options;
-        options.nodes=$A([]);
-        if (nodes && nodes.length > 0) {
-            for (var i = 0; i < nodes.length; i++) {
-                options.nodes[options.nodes.length] = new MUI.TreeItem(nodes[i],this.Tree,o);
-            }
-        }
-        this.options=options;
-    },
-
-    toDOM: function(nosave, l, depth) {
-        var o = this;
-        var options=this.options;
-        var topt=this.Tree.options;
-        if(!depth) depth=1;
-
-        var a, s, t, u, chk;
-        if(l==null && o.DOM!=null) {l=o.DOM;}
-        if(!l) l = new Element('li');
-        else l.empty();
-
-        chk = o.createField('checkbox',options.value,'');
-        a = new Element('a');
-        s = new Element('span');
-        t = document.createTextNode(options.text);
-        l.appendChild(a);
-        a.appendChild(s);
-        s.appendChild(t);
-
-        if (!nosave) o.DOM = l;
-        if (a) {
-            if (options.tip) a.title = options.tip;
-            a.href = '#' + options.value;
-        }
-
-        if (chk) chk.checked = options.checked;
-
-        if (o.Tree && o.Tree.options.SelectedValue == options.value) {
-            o.Tree.DOM.getElements('.sel').removeClass('sel');
-            if (a) a.className = 'sel';
-            o.Tree.options.SelectedNode = o;
-        }
-
-        if (options.image) {
-            s.style.background = 'transparent url(' + options.image + ') no-repeat scroll left top';
-            s.style.paddingLeft = '20px';
-        }
-
-        if (options.nodes && options.nodes.length > 0) {
-            if (!u) {
-                u = new Element('ul');
-                l.appendChild(u);
-            }
-            if (options.expanded == 0 && topt.Depth<=depth) u.style.display = 'none';
-
-            for (var i = 0; i < options.nodes.length; i++) {
-                u.appendChild(options.nodes[i].toDOM(false,false,depth+1));
-            }
-            u.childNodes[u.childNodes.length - 1].addClass('last');
-        }
-        if (options.expanded != 0) l.className = 'O';
-        else l.className = 'C';
-        if ((!options.nodes || options.nodes.length == 0) && options.hasChildren == 0) l.className = 'nochild';
-
-        o.setEvents();
-
-        return l;
-    },
-
-    toHTML: function() {
-        var e = new Element('div');
-        e.appendChild(this.toDOM(true));
-        return e.innerHTML;
-    },
-
-    expand: function(e) {
-        var o = this;
-        var options=o.options;
-
-        var itm=o.DOM;
-        var mY=0;
-        var mX=0;
-        if(e) {
-            e = new Event(e);
-            e.stop();
-            var c = itm.getCoordinates();
-            mY = e.client.y - c.top;
-            mX = e.client.x - c.left;
-        }
-
-        if (mX < 20 && mY < 20) {
-            var last = '';
-            if (itm.className.indexOf('last') > -1) last = ' last';
-            var ul = itm.getElement('ul');
-            if (ul && options.expanded == 1) {
-                ul.style.display = 'none';
-                itm.className = 'C' + last;
-                options.expanded = 0;
-                return;
-            }
-            else if (ul && options.expanded == 0) {
-                ul.style.display = '';
-                itm.className = 'O' + last;
-                options.expanded = 1;
-                if (options.nodes.length > 0) return;
-            }
-        }
-
-        if(options.hasChildren == 1 && options.nodes.length == 0) {
-            o.Tree.fireEvent('onNodeExpanded',[e,o]);
-        }
-        o.Tree.selectValue(o.options.value,e);
-    },
-
-    click: function(e) {
-        var o = this;
-        e=new Event(e);
-        e.stop();
-        o.Tree.selectValue(o.options.value,e);
-    },
-
-    check: function(e) {
-        var o = this;
-        e=new Event(e);
-        e.stop();
-        o.Tree.fireEvent('onNodeChecked',[e,o]);
-    },
-
-    setEvents: function() {
-        var o = this;
-        if (o.DOM) {
-            var li = $(o.DOM);
-            var a = li.getElement('a');
-            var inp = li.getElement('input');
-            if (!a) return;
-
-            if (li) {
-                if (inp) {
-                    inp.removeEvents('click');
-                    inp.addEvent('click', function(e) { new Event(e).stop(); o.check(e) });
-                }
-                li.removeEvents('click');
-                li.addEvent('click', function(e) { new Event(e).stop(); o.expand(e) });
-                a.removeEvents('click');
-                a.addEvent('click', function(e) { new Event(e).stop(); o.click(e) });
-            }
-        }
-    },
-
-    refreshNode: function() {
-        this.toDOM();
-        this.Tree.setLast(this.DOM.getParent());
-    },
-
-    getPath: function() {
-        var o=this;
-        if(!o.ParentNode) return o.options.value;
-        return o.ParentNode.getPath()+'/'+o.options.value;
+    var itm=self._element;
+    var mY=0;
+    var mX=0;
+    if(e) {
+        e = new Event(e).stop();
+        var c = itm.getCoordinates();
+        mY = e.client.y - c.top;
+        mX = e.client.x - c.left;
     }
+
+    if (mX < 20 && mY < 20) {
+        itm.addClass('last');
+        var ul = itm.getElement('ul');
+        if (ul && o.expanded == 1) {
+            ul.style.display = 'none';
+            itm.removeClass('O');
+            itm.addClass('C');
+            node.expanded = 0;
+            return;
+        }
+        else if (ul && o.expanded == 0) {
+            ul.style.display = '';
+            itm.removeClass('C');
+            itm.addClass('O');
+            node.expanded = 1;
+            if (node.nodes.length > 0) return;
+        }
+    }
+
+    if(o.hasChildren == 1 && o.nodes.length == 0) {
+        self.fireEvent('nodeExpanded',[e,self]);
+    }
+    self.selectValue(node.value,e);
+},
+
+onNodeClick: function(node,e) {
+    var self = this;
+    e=new Event(e).stop();
+    self.selectValue(node.value,e);
+},
+
+onNodeCheck: function(node,e) {
+    var self = this;
+    e=new Event(e).stop();
+    self.fireEvent('nodeChecked',[node,self,e]);
+}
+
 });
-*/
