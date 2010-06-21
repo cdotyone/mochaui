efeff3b6cf073cf12ae43e695d0306f9233daabe
Chris Doty <cdoty1@yahoo.com>
2010-06-21 02:43:17 -0500

    adjust updatecontent to allow for user defined content provider routines

--- src/mui/update.js
+++ src/mui/update.js
@@ -23,6 +23,8 @@ MUI.files[MUI.path.source + 'update.js'] = 'loaded';
 
 MUI.extend({
 
+	Content:{Providers:{}},
+
 	/*
 
 	 Function: updateContent
@@ -142,184 +144,161 @@ MUI.extend({
 				css: options.require.css,
 				images: options.require.images,
 				onload: function(){
-					this.loadSelect(instance, options, args);
+					MUI.Content.Providers[options.loadMethod](instance, options, args);
 				}.bind(this)
 			});
+		} else {
+			MUI.Content.Providers[options.loadMethod](instance, options, args);
 		}
-		else {
-			this.loadSelect(instance, options, args);
-		}
-	},
+	}
+});
 
-	loadSelect: function(instance, options, args){
+MUI.Content.Providers.xhr = function(instance, options, args){
+	var contentEl = instance.contentEl;
+	var contentContainer = args.contentContainer;
+	var onContentLoaded = args.onContentLoaded;
+	new Request.HTML({
+		url: options.url,
+		update: contentContainer,
+		method: options.method != null ? options.method : 'get',
+		data: options.data != null ? new Hash(options.data).toQueryString() : '',
+		evalScripts: instance.options.evalScripts,
+		evalResponse: instance.options.evalResponse,
+		onRequest: function(){
+			if (args.recipient == 'window' && contentContainer == contentEl){
+				instance.showSpinner();
+			}
+			else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')){
+				$('spinner').show();
+			}
+		}.bind(this),
+		onFailure: function(response){
+			if (contentContainer == contentEl){
+				var getTitle = new RegExp('<title>[\n\r\s]*(.*)[\n\r\s]*</title>', 'gmi');
+				var error = getTitle.exec(response.responseText);
+				if (!error) error = 'Unknown';
+				contentContainer.set('html', '<h3>Error: ' + error[1] + '</h3>');
+				if (args.recipient == 'window'){
+					instance.hideSpinner();
+				}
+				else if (args.recipient == 'panel' && $('spinner')){
+					$('spinner').hide();
+				}
+			}
+		}.bind(this),
+		onSuccess: function(){
+			contentEl.addClass('pad');
+			if (contentContainer == contentEl){
+				if (args.recipient == 'window') instance.hideSpinner();
+				else if (args.recipient == 'panel' && $('spinner')) $('spinner').hide();
+			}
+			Browser.Engine.trident4 ? onContentLoaded.delay(750) : onContentLoaded();
+		}.bind(this),
+		onComplete: function(){
+		}.bind(this)
+	}).send();
+};
 
-		// Load new content.
-		switch (options.loadMethod){
-			case 'xhr':
-				this.updateContentXHR(instance, options, args);
-				break;
-			case 'iframe':
-				this.updateContentIframe(instance, options, args);
-				break;
-			case 'json':
-				this.updateContentJSON(instance, options, args);
-				break;
-			case 'html':
-			default:
-				this.updateContentHTML(instance, options, args);
-				break;
-		}
+MUI.Content.Providers.json = function(instance, options, args){
+	var contentEl = instance.contentEl;
+	var contentContainer = args.contentContainer;
 
-	},
-
-	updateContentJSON: function(instance, options, args){
-		var contentEl = instance.contentEl;
-		var contentContainer = args.contentContainer;
-
-		new Request({
-			url: options.url,
-			update: contentContainer,
-			method: options.method != null ? options.method : 'get',
-			data: options.data != null ? new Hash(options.data).toQueryString() : '',
-			evalScripts: false,
-			evalResponse: false,
-			headers: {'Content-Type':'application/json'},
-			onRequest: function(){
-				if (args.recipient == 'window' && contentContainer == contentEl){
-					instance.showSpinner();
+	new Request({
+		url: options.url,
+		update: contentContainer,
+		method: options.method != null ? options.method : 'get',
+		data: options.data != null ? new Hash(options.data).toQueryString() : '',
+		evalScripts: false,
+		evalResponse: false,
+		headers: {'Content-Type':'application/json'},
+		onRequest: function(){
+			if (args.recipient == 'window' && contentContainer == contentEl){
+				instance.showSpinner();
+			}
+			else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')){
+				$('spinner').show();
+			}
+		}.bind(this),
+		onFailure: function(){
+			if (contentContainer == contentEl){
+				contentContainer.set('html', '<p><strong>Error Loading XMLHttpRequest</strong></p>');
+				if (args.recipient == 'window'){
+					instance.hideSpinner();
 				}
-				else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')){
-					$('spinner').show();
+				else if (args.recipient == 'panel' && $('spinner')){
+					$('spinner').hide();
 				}
-			}.bind(this),
-			onFailure: function(){
-				if (contentContainer == contentEl){
-					contentContainer.set('html', '<p><strong>Error Loading XMLHttpRequest</strong></p>');
-					if (args.recipient == 'window'){
-						instance.hideSpinner();
-					}
-					else if (args.recipient == 'panel' && $('spinner')){
-						$('spinner').hide();
-					}
-				}
-			}.bind(this),
-			onException: function(){
-			}.bind(this),
-			onSuccess: function(json){
-				if (contentContainer == contentEl){
-					if (contentContainer == contentEl){
-						if (args.recipient == 'window') instance.hideSpinner();
-						else if (args.recipient == 'panel' && $('spinner')) $('spinner').hide();
-					}
-					json = JSON.decode(json);
-					// calls onLoaded event instead of onContentLoaded
-					// onLoaded - event should call updateContent again with loadMethod='html'
-					instance.fireEvent('loaded', $A([options.element, json, instance]));
-				}
-			}.bind(this),
-			onComplete: function(){
-			}.bind(this)
-		}).get();
-	},
-
-	updateContentXHR: function(instance, options, args){
-		var contentEl = instance.contentEl;
-		var contentContainer = args.contentContainer;
-		var onContentLoaded = args.onContentLoaded;
-		new Request.HTML({
-			url: options.url,
-			update: contentContainer,
-			method: options.method != null ? options.method : 'get',
-			data: options.data != null ? new Hash(options.data).toQueryString() : '',
-			evalScripts: instance.options.evalScripts,
-			evalResponse: instance.options.evalResponse,
-			onRequest: function(){
-				if (args.recipient == 'window' && contentContainer == contentEl){
-					instance.showSpinner();
-				}
-				else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')){
-					$('spinner').show();
-				}
-			}.bind(this),
-			onFailure: function(response){
-				if (contentContainer == contentEl){
-					var getTitle = new RegExp('<title>[\n\r\s]*(.*)[\n\r\s]*</title>', 'gmi');
-					var error = getTitle.exec(response.responseText);
-					if (!error) error = 'Unknown';
-					contentContainer.set('html', '<h3>Error: ' + error[1] + '</h3>');
-					if (args.recipient == 'window'){
-						instance.hideSpinner();
-					}
-					else if (args.recipient == 'panel' && $('spinner')){
-						$('spinner').hide();
-					}
-				}
-			}.bind(this),
-			onSuccess: function(){
-				contentEl.addClass('pad');
+			}
+		}.bind(this),
+		onException: function(){
+		}.bind(this),
+		onSuccess: function(json){
+			if (contentContainer == contentEl){
 				if (contentContainer == contentEl){
 					if (args.recipient == 'window') instance.hideSpinner();
 					else if (args.recipient == 'panel' && $('spinner')) $('spinner').hide();
 				}
-				Browser.Engine.trident4 ? onContentLoaded.delay(750) : onContentLoaded();
-			}.bind(this),
-			onComplete: function(){
-			}.bind(this)
-		}).send();
-	},
-
-	updateContentIframe: function(instance, options, args){
-		var contentEl = instance.contentEl;
-		var contentContainer = args.contentContainer;
-		var contentWrapperEl = instance.contentWrapperEl;
-		var onContentLoaded = args.onContentLoaded;
-		if (instance.options.contentURL == '' || contentContainer != contentEl){
-			return;
-		}
-		contentEl.removeClass('pad');
-		contentEl.setStyle('padding', '0px');
-		instance.iframeEl = new Element('iframe', {
-			'id': instance.options.id + '_iframe',
-			'name': instance.options.id + '_iframe',
-			'class': 'mochaIframe',
-			'src': options.url,
-			'marginwidth': 0,
-			'marginheight': 0,
-			'frameBorder': 0,
-			'scrolling': 'auto',
-			'styles': {
-				'height': contentWrapperEl.offsetHeight - contentWrapperEl.getStyle('border-top').toInt() - contentWrapperEl.getStyle('border-bottom').toInt(),
-				'width': instance.panelEl ? contentWrapperEl.offsetWidth - contentWrapperEl.getStyle('border-left').toInt() - contentWrapperEl.getStyle('border-right').toInt() : '100%'
+				json = JSON.decode(json);
+				// calls onLoaded event instead of onContentLoaded
+				// onLoaded - event should call updateContent again with loadMethod='html'
+				instance.fireEvent('loaded', $A([options.element, json, instance]));
 			}
-		}).injectInside(contentEl);
+		}.bind(this),
+		onComplete: function(){
+		}.bind(this)
+	}).get();
+};
 
-		// Add onload event to iframe so we can hide the spinner and run onContentLoaded()
-		instance.iframeEl.addEvent('load', function(){
-			if (args.recipient == 'window') instance.hideSpinner();
-			else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')) $('spinner').hide();
-			Browser.Engine.trident4 ? onContentLoaded.delay(50) : onContentLoaded();
-		}.bind(this));
-		if (args.recipient == 'window') instance.showSpinner();
-		else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')) $('spinner').show();
-	},
-
-	updateContentHTML: function(instance, options, args){
-		var contentEl = instance.contentEl;
-		var contentContainer = args.contentContainer;
-		var onContentLoaded = args.onContentLoaded;
-		var elementTypes = new Array('element', 'textnode', 'whitespace', 'collection');
-
-		contentEl.addClass('pad');
-		if (elementTypes.contains($type(options.content))){
-			options.content.inject(contentContainer);
-		} else {
-			contentContainer.set('html', options.content);
-		}
-		if (contentContainer == contentEl){
-			if (args.recipient == 'window') instance.hideSpinner();
-			else if (args.recipient == 'panel' && $('spinner')) $('spinner').hide();
-		}
-		Browser.Engine.trident4 ? onContentLoaded.delay(50) : onContentLoaded();
+MUI.Content.Providers.iframe = function(instance, options, args){
+	var contentEl = instance.contentEl;
+	var contentContainer = args.contentContainer;
+	var contentWrapperEl = instance.contentWrapperEl;
+	var onContentLoaded = args.onContentLoaded;
+	if (instance.options.contentURL == '' || contentContainer != contentEl){
+		return;
 	}
+	contentEl.removeClass('pad');
+	contentEl.setStyle('padding', '0px');
+	instance.iframeEl = new Element('iframe', {
+		'id': instance.options.id + '_iframe',
+		'name': instance.options.id + '_iframe',
+		'class': 'mochaIframe',
+		'src': options.url,
+		'marginwidth': 0,
+		'marginheight': 0,
+		'frameBorder': 0,
+		'scrolling': 'auto',
+		'styles': {
+			'height': contentWrapperEl.offsetHeight - contentWrapperEl.getStyle('border-top').toInt() - contentWrapperEl.getStyle('border-bottom').toInt(),
+			'width': instance.panelEl ? contentWrapperEl.offsetWidth - contentWrapperEl.getStyle('border-left').toInt() - contentWrapperEl.getStyle('border-right').toInt() : '100%'
+		}
+	}).injectInside(contentEl);
 
-});
+	// Add onload event to iframe so we can hide the spinner and run onContentLoaded()
+	instance.iframeEl.addEvent('load', function(){
+		if (args.recipient == 'window') instance.hideSpinner();
+		else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')) $('spinner').hide();
+		Browser.Engine.trident4 ? onContentLoaded.delay(50) : onContentLoaded();
+	}.bind(this));
+	if (args.recipient == 'window') instance.showSpinner();
+	else if (args.recipient == 'panel' && contentContainer == contentEl && $('spinner')) $('spinner').show();
+};
+
+MUI.Content.Providers.html = function(instance, options, args){
+	var contentEl = instance.contentEl;
+	var contentContainer = args.contentContainer;
+	var onContentLoaded = args.onContentLoaded;
+	var elementTypes = new Array('element', 'textnode', 'whitespace', 'collection');
+
+	contentEl.addClass('pad');
+	if (elementTypes.contains($type(options.content))){
+		options.content.inject(contentContainer);
+	} else {
+		contentContainer.set('html', options.content);
+	}
+	if (contentContainer == contentEl){
+		if (args.recipient == 'window') instance.hideSpinner();
+		else if (args.recipient == 'panel' && $('spinner')) $('spinner').hide();
+	}
+	Browser.Engine.trident4 ? onContentLoaded.delay(50) : onContentLoaded();
+};
