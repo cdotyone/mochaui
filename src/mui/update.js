d577ffda2d3e616a9195b6e5024a14c448964bd7
Chris Doty <cdoty1@yahoo.com>
2010-06-21 02:45:48 -0500

    description of new updateContent workflow intercept functionality

--- src/mui/update.js
+++ src/mui/update.js
@@ -21,6 +21,35 @@
 
 MUI.files[MUI.path.source + 'update.js'] = 'loaded';
 
+/*
+
+updateContent allows controls, windows, and panels to intercept the standard update process.
+By adding the following functions to the classes that define them, they can be intercepted.
+
+In each case the expected return result is a boolean value.  A return result True tells
+updateContent to perform the standard functionality that it would normally.  A value of
+False tells updateContent that the standard functionality can be bypassed.
+
+Each of the functions will be passed the options hash for the initial call to updateContent.
+
+instance.updateStart - this is called when updateContent first starts, it is designed to allow the
+	instance the ability to set things like titles and scrollbars.  The return result is ignored
+	at this point.
+
+instance.updateClear - this is called when updateContent needs to clear the contents of
+	the childElement.  The return result true/false will determine if the childElement is
+	cleared by updateContent.
+
+instance.updateContent - this is called after a response has been received and the content of the
+	childElement needs to be updated.  The return result true/false will determine if the
+	childElement is updated by updateContent.
+
+instance.updateEnd - this is called after the childElement has been updated and the control,
+	window, or panel needs to be informed that the update was successful.  The return result
+	true/false will determine if control, window, or panel will be informed of the update.
+
+*/
+
 MUI.extend({
 
 	Content:{Providers:{}},
