bb4824e9ad937ad6d92decd5716d01435e948f72
Chris Doty <cdoty1@yahoo.com>
2010-06-22 00:20:34 -0500

    fix license and author windows in virtual desktop

--- src/demo/scripts/demo-virtual-desktop-init.js
+++ src/demo/scripts/demo-virtual-desktop-init.js
@@ -490,6 +490,41 @@ initializeWindows = function(){
         });
     }
 
+    // Misc
+    MUI.authorsWindow = function(){
+        new MUI.Modal({
+            id: 'authorsWindow',
+            title: 'AUTHORS.txt',
+            contentURL: 'scripts/AUTHORS.txt',
+            width: 400,
+            height: 250,
+            scrollbars:true
+        });
+    };
+    if ($('authorsLink')){
+        $('authorsLink').addEvent('click', function(e){
+            new Event(e).stop();
+            MUI.authorsWindow();
+        });
+    }
+
+    MUI.licenseWindow = function(){
+        new MUI.Modal({
+            id: 'License',
+            title: 'MIT-LICENSE.txt',
+            contentURL: 'scripts/MIT-LICENSE.txt',
+            width: 580,
+            height: 350,
+            scrollbars:true
+        });
+    };
+    if ($('licenseLink')){
+        $('licenseLink').addEvent('click', function(e){
+            new Event(e).stop();
+            MUI.licenseWindow();
+        });
+    }
+    
     // Deactivate menu header links
     $$('a.returnFalse').addEvent('click', function(e){
 		e.stop();
