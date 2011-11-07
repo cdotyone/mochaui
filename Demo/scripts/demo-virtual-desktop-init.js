05e384512e29a546eb54d56add67d9fcb1fa9c14
cdotyone <cdotyone@gmail.com>
2011-11-07 00:34:26 -0600

    bringing virtual desktop back alive.  almost there.  not happy with taskbar.

--- Demo/scripts/demo-virtual-desktop-init.js
+++ Demo/scripts/demo-virtual-desktop-init.js
@@ -12,218 +12,23 @@ var Demo = (Demo || {});
 
 Demo.initializeDesktop = function(){
 
-	MUI.create({
-		'control':'MUI.Desktop',
-		'id':'desktop',
-		'taskbar':true,
-		'content':[
-
 	// change default setting - keep window within inside the main area.
 	MUI.Windows.options.container = 'pageWrapper';
 
-	// Examples
-	if ($('ajaxpageLinkCheck')){
-		$('ajaxpageLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.ajaxpageWindow();
-		});
-	}
+	MUI.register('Demo', Demo);
+	MUI.register('MUI.Windows', MUI.Windows);
 
-	if ($('jsonLink')){
-		$('jsonLink').addEvent('click', function(e){
-			e.stop();
-			Demo.jsonWindows();
-		});
-	}
-
-	if ($('youtubeLinkCheck')){
-		$('youtubeLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.youtubeWindow();
-		});
-	}
-
-	if ($('clockLinkCheck')){
-		$('clockLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.clockWindow();
-		});
-	}
-
-	if ($('parametricsLinkCheck')){
-		$('parametricsLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.parametricsWindow();
-		});
-	}
-
-	// Examples > Tests
-	if ($('windoweventsLinkCheck')){
-		$('windoweventsLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.eventsWindow();
-		});
-	}
-
-	MUI.containerTestWindow = function(){
-		new MUI.Window({
-			id: 'containertest',
-			title: 'Container Test',
-			loadMethod: 'xhr',
-			content: {url: 'pages/lipsum.html'},
-			container: 'pageWrapper',
-			width: 340,
-			height: 150,
-			x: 100,
-			y: 100
-		});
-	};
-	if ($('containertestLinkCheck')){
-		$('containertestLinkCheck').addEvent('click', function(e){
-			e.stop();
-			MUI.containerTestWindow();
-		});
-	}
-
-	if ($('iframetestLinkCheck')){
-		$('iframetestLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.iframeTestsWindow();
-		});
-	}
-
-	if ($('accordiontestLinkCheck')){
-		$('accordiontestLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.accordionBuilder();
-		});
-	}
-
-	if ($('noCanvasLinkCheck')){
-		$('noCanvasLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.noCanvasWindow();
-		});
-	}
-
-	// View
-	if ($('sidebarLinkCheck')){
-		$('sidebarLinkCheck').addEvent('click', function(e){
-			e.stop();
-			MUI.Desktop.sidebarToggle();
-		});
-	}
-
-	if ($('cascadeLink')){
-		$('cascadeLink').addEvent('click', function(e){
-			e.stop();
-			MUI.Windows.arrangeCascade();
-		});
-	}
-
-	if ($('tileLink')){
-		$('tileLink').addEvent('click', function(e){
-			e.stop();
-			MUI.Windows.arrangeTile();
-		});
-	}
-
-	if ($('closeLink')){
-		$('closeLink').addEvent('click', function(e){
-			e.stop();
-			MUI.Windows.closeAll();
-		});
-	}
-
-	if ($('minimizeLink')){
-		$('minimizeLink').addEvent('click', function(e){
-			e.stop();
-			MUI.Windows.minimizeAll();
-		});
-	}
-
-	// Tools
-	if ($('builderLinkCheck')){
-		$('builderLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.builderWindow();
-		});
-	}
-
-	// Todo: Add menu check mark functionality for workspaces.
-
-	// Workspaces
-
-	if ($('saveWorkspaceLink')){
-		$('saveWorkspaceLink').addEvent('click', function(e){
-			e.stop();
-			Demo.saveWorkspace();
-		});
-	}
-
-	if ($('loadWorkspaceLink')){
-		$('loadWorkspaceLink').addEvent('click', function(e){
-			e.stop();
-			Demo.loadWorkspace();
-		});
-	}
-
-	if ($('toggleStdEffectsLinkCheck')){
-		$('toggleStdEffectsLinkCheck').addEvent('click', function(e){
-			e.stop();
-			MUI.toggleStandardEffects($('toggleStdEffectsLinkCheck'));
-		});
-	}
-
-	if ($('toggleAdvEffectsLinkCheck')){
-		$('toggleAdvEffectsLinkCheck').addEvent('click', function(e){
-			e.stop();
-			MUI.toggleAdvancedEffects($('toggleAdvEffectsLinkCheck'));
-		});
-	}
-
-	// Help
-	if ($('featuresLinkCheck')){
-		$('featuresLinkCheck').addEvent('click', function(e){
-			e.stop();
-			Demo.featuresWindow();
-		});
-	}
-
-	if ($('aboutLink')){
-		$('aboutLink').addEvent('click', function(e){
-			e.stop();
-			Demo.aboutWindow();
-		});
-	}
-
-	// Misc
-	if ($('authorsLink')){
-		$('authorsLink').addEvent('click', function(e){
-			new Event(e).stop();
-			Demo.authorsWindow();
-		});
-	}
-
-	if ($('licenseLink')){
-		$('licenseLink').addEvent('click', function(e){
-			new Event(e).stop();
-			Demo.licenseWindow();
-		});
-	}
-
-	// Deactivate menu header links
-	$$('a.returnFalse').addEvent('click', function(e){
-		e.stop();
+	MUI.create({
+		'control':'MUI.Desktop',
+		'id':'desktop',
+		'fromHTML':true,
+		'onDrawEnd':function() {
+			// This is just for the demo. Running it onload gives pngFix time to replace the pngs in IE6.
+			$$('.desktopIcon').addEvent('click', function(){
+				MUI.notification('Do Something');
+			});
+		}
 	});
-
-	if (0)
-	{
-		// Build windows onLoad
-		Demo.parametricsWindow();
-		Demo.clockWindow();
-		MUI.myChain.callChain();
-	}
 };
 
 Demo.initialize = function(){
@@ -232,14 +37,13 @@ Demo.initialize = function(){
 		'onload':function(){
 			// Initialize MochaUI options
 			MUI.initialize({path:{demo:''}});
-			MUI.load(['Parametrics','famfamfam','CoolClock','WindowForm'], function(load_options) {
+			MUI.load(['MUI.Window','MUI.Desktop','Parametrics','famfamfam','CoolClock','WindowForm'], function(load_options) {
 				if (window.console && window.console.log)
 					window.console.log('MUI.load event: ', load_options, ', arguments (', arguments.length, '): ', arguments);
+				Demo.initializeDesktop();
 			});
-			MUI.register('Demo', Demo);
-			MUI.register('MUI.Windows', MUI.Windows);
-			Demo.initializeDesktop();
 
+/*
 			// force checkbox on menu to be in correct state
 			MUI.options.standardEffects = !MUI.options.standardEffects;
 			MUI.toggleStandardEffects($('toggleStdEffectsLinkCheck'));
@@ -247,13 +51,9 @@ Demo.initialize = function(){
 			// force checkbox on menu to be in correct state
 			MUI.options.advancedEffects = !MUI.options.advancedEffects;
 			MUI.toggleAdvancedEffects($('toggleAdvEffectsLinkCheck'));
-
-			// This is just for the demo. Running it onload gives pngFix time to replace the pngs in IE6.
-			$$('.desktopIcon').addEvent('click', function(){
-				MUI.notification('Do Something');
+*/
 		}
 	});
-
 };
 
 // Initialize MochaUI when the DOM is ready
