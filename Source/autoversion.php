8aae6ab98af2165ca573cceeb1886fb669ed3b00
Roberto Fernandez <robernet@gmail.com>
2014-10-11 05:16:17 -0500

    Allows autoVersion handle arrays in POST

--- Source/autoversion.php
+++ Source/autoversion.php
@@ -7,6 +7,13 @@
 // Inputs: file=ui/scripts/mochaui.js
 // Returns: ui/scripts/mochaui.1410233302.js
 //
+// Inputs: file[]=file1.js&file[]=file2.js&file[]=fileN.js
+// Returns: ['file1.1309872290.js',file2.1987309234.js','fileN.1872098368.js']
+//
+// Note: The last is only useful in projects outside MochaUI that needs to load
+//	several JS files at once, since MochaUI handles this one by one
+//	in require_with_versioning.js.
+//
 // Example: autoversion.php?file=ui/scripts/mochaui.js
 //
 // Created by: Roberto Fernandez (RoberNET)
@@ -20,5 +27,15 @@ function autoVersion($url){
 	return $path['dirname'].'/'.str_replace('.', $version, $path['basename']);
 }
 
-if (isset($_REQUEST["file"])) echo autoVersion($_REQUEST["file"]);
+if (isset($_REQUEST["file"])) {
+	if (is_array($_REQUEST["file"])) {
+		$strVersion = "[";
+		foreach($_REQUEST["file"] as $key => $value) {
+			if ($strVersion!="[") $strVersion .= ",";
+			$strVersion.= "'".autoVersion($value)."'";
+		}
+		$strVersion .= "]";
+		echo $strVersion;
+	} else echo autoVersion($_REQUEST["file"]);
+}
 ?>
